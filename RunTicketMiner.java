17a8f6a79e33253e26ca183939a1bd2a900020fa
Leo's Uni Repo <lahernandez30@miners.utep.edu>
2026-03-06 01:51:56 -0700

    feat(main): expanded user interface and started the register section of the menu
    fix: control flow errors

--- RunTicketMiner.java
+++ RunTicketMiner.java
@@ -7,47 +7,91 @@ import java.util.*;
  * @author Kevin Pinon
  */
 public class RunTicketMiner {
+    /** The current user as determined by who the user registers or logs in as. */
     User currentUser;
+    /** Accepts input from the user in the terminal */
+    private static final Scanner input = new Scanner(System.in);
 
+    /**
+     * Initializes all class variables, starts listening to user input in terminal
+     * using the input Scanner, and calls the mainMenu method that provides GUI for
+     * the user.
+     */
     public static void main(String[] args) {
-        // Leo: Leaving this empty until other classes are created (the errors are
-        // driving me insane)
-        Scanner input = new Scanner(System.in);
 
-        System.out.println("[Welcome to TicketMiner]");
-        System.out.println("Register ---> r");
-        System.out.println("Login ------> l");
-        System.out.println("Exit -------> EXIT");
-        
+        mainMenu();
+        input.close();
+    }
 
+    /** Prints the main menu and listens to user input via the terminal. */
+    public static void mainMenu() {
+        /** ask whether the program has been terminated. */
         boolean isTerminated = false;
+        /** asks whether the main menu needs to be printed again. */
+        boolean printMainMenu = true;
+
         while (!isTerminated) {
-            System.out.print(">> ");
+            if (printMainMenu) {
+                System.out.println("[Welcome to TicketMiner]");
+                System.out.println("Register ---> r");
+                System.out.println("Login ------> l");
+                System.out.println("Exit -------> EXIT");
+                System.out.print(">> ");
+                printMainMenu = false;
+            }
             switch (input.next()) {
                 case "r", "R" -> {
+                    registerMenu();
+                    printMainMenu = true;
                 }
                 case "l", "L" -> {
                 }
-                case "exit" -> System.out.println("Input invalid. Did you mean \"EXIT\"?");
+                case "exit", "Exit" -> System.out.println("Input invalid. Did you mean \"EXIT\"?");
                 case "EXIT" -> {
                     isTerminated = true;
-                    input.close();
                 }
-                default -> System.out.println("Input invalid.");
+                default -> {
+                    System.out.println("Input invalid.");
+                }
             }
         }
     }
 
+    /** Prints register menu options and listens to user input via the terminal. */
+    public static void registerMenu() {
+
+        System.out.println("[What would you like to register as?]");
+        System.out.println("As a Customer --------> c");
+        System.out.println("As an Organizer ------> o");
+        System.out.println("Back -----------------> b");
+
+        /** asks if we can stop looping the user interface listener and go back to the main menu */
+        boolean goBack = false;
+        while (!goBack) {
+            System.out.print(">> ");
+            switch (input.next()) {
+                case "c", "C" -> {
+                }
+                case "o", "O" -> {
+
+                }
+                case "b", "B" -> goBack = true;
+                default -> System.out.println("Input invalid");
+            }
+        }
+        System.out.println("");
+    }
+
     /*
-    public void loadData() {
-    }
-    */
-   /*
-    public void saveData() {
-    }
-    */
-   /*
-    public void logAction(String action) {
-    }
-    */
+     * public void loadData() {
+     * }
+     */
+    /*
+     * public void saveData() {
+     * }
+     */
+    /*
+     * public void logAction(String action) {
+     * }
+     */
 }
