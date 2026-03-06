e8e378ba394144d9e7b1c3d175a2b3058bff518a
Leo's Uni Repo <lahernandez30@miners.utep.edu>
2026-03-06 00:04:50 -0700

    feat(main): created basic user interface and set it up to call methods into different "menu" methods

--- RunTicketMiner.java
+++ RunTicketMiner.java
@@ -1,5 +1,53 @@
+import java.util.*;
+
+/**
+ * TicketMiner's user interface and loading/saving information mechanism.
+ * 
+ * @author Leonardo Andree Hernandez
+ * @author Kevin Pinon
+ */
 public class RunTicketMiner {
-    public static void main(String [] args) {
-        // Leo: Leaving this empty until other classes are created (the errors are driving me insane)
+    User currentUser;
+
+    public static void main(String[] args) {
+        // Leo: Leaving this empty until other classes are created (the errors are
+        // driving me insane)
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("[Welcome to TicketMiner]");
+        System.out.println("Register ---> r");
+        System.out.println("Login ------> l");
+        System.out.println("Exit -------> EXIT");
+        
+
+        boolean isTerminated = false;
+        while (!isTerminated) {
+            System.out.print(">> ");
+            switch (input.next()) {
+                case "r", "R" -> {
+                }
+                case "l", "L" -> {
+                }
+                case "exit" -> System.out.println("Input invalid. Did you mean \"EXIT\"?");
+                case "EXIT" -> {
+                    isTerminated = true;
+                    input.close();
+                }
+                default -> System.out.println("Input invalid.");
+            }
+        }
     }
+
+    /*
+    public void loadData() {
+    }
+    */
+   /*
+    public void saveData() {
+    }
+    */
+   /*
+    public void logAction(String action) {
+    }
+    */
 }
