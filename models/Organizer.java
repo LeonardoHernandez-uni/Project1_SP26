0a1a4d14ba5294edb94da7cefd8f9fbeba78e062
Leo's Uni Repo <lahernandez30@miners.utep.edu>
2026-03-08 15:02:32 -0600

    fix(Organizer): deleted organizerName attribute as it was irrelevant to coursework

--- models/Organizer.java
+++ models/Organizer.java
@@ -1,10 +1,8 @@
 package models;
 public class Organizer extends User {
-    String organizationName;
 
-    public Organizer(String firstName, String lastName, String username, String password, int userID, String organizationName) {
+    public Organizer(String firstName, String lastName, String username, String password, int userID) {
         super(firstName, lastName, username, password, userID);
-        this.organizationName = organizationName;
     }
 
     public void createEvent() {
